ca6171706a5d52b18a7e652f7c0bbf05da754848
Keryyyl <karol.idzkowski06@gmail.com>
2025-03-03 17:16:04 +0100

    Adds Loading Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import './App.css'
+import { useState } from 'react';
+import { LoadingScreen } from './components/LoadingScreen'
+import "./index.css";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
     <>
+     {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
     </>
   )
 }
