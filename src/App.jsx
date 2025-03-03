ee2447d4d431033b75b0a5f84846a988ad5e3392
Keryyyl <karol.idzkowski06@gmail.com>
2025-03-03 23:07:39 +0100

    Adds Contact Form and Handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import "./index.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
-// import { Contact } from "./components/sections/Contact";
+import { Contact } from "./components/sections/Contact";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    window.history.scrollRestoration = 'manual'
+  }, []);
 
   return (
     <>
@@ -25,7 +29,7 @@ function App() {
         <Home />
         <About />
         <Projects />
-        {/* <Contact /> */}
+        <Contact />
       </div>
     </>
   )
