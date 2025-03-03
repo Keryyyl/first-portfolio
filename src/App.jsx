cd5e27d1e993321f3b1c0be3773d597afa0a47a1
Keryyyl <karol.idzkowski06@gmail.com>
2025-03-03 20:28:06 +0100

    Adds about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
-// import { About } from "./components/sections/About";
+import { About } from "./components/sections/About";
 // import { Projects } from "./components/sections/Projects";
 // import { Contact } from "./components/sections/Contact";
 
@@ -22,9 +22,9 @@ function App() {
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-         <Home />
-        {/*<About />
-        <Projects />
+        <Home />
+        <About />
+        {/*<Projects />
         <Contact /> */}
       </div>
     </>
