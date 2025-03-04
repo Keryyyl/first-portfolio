689a3e58ac0702f0225d49253eba50d0592120fa
Karol <92820374+Keryyyl@users.noreply.github.com>
2025-03-04 12:19:18 +0100

    Clears Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 
+const MenuData = ["home", "about", "projects", "contact"];
+
 export const Navbar = ({ menuOpen, setMenuOpen }) => {
   useEffect(() => {
     document.body.style.overflow = menuOpen ? "hidden" : "";
@@ -16,18 +18,9 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
             &#9776;
           </div>
           <div className="hidden md:flex items-center space-x-8">
-            <a href="#home" className="text-gray-300 hover:text-white transition-colors">
-              Home
-            </a>
-            <a href="#about" className="text-gray-300 hover:text-white transition-colors">
-              About
-            </a>
-            <a href="#projects" className="text-gray-300 hover:text-white transition-colors">
-              Projects
-            </a>
-            <a href="#contact" className="text-gray-300 hover:text-white transition-colors">
-              Contact
-            </a>
+            {MenuData.map((text, index) => {
+              <a key={index} href={`#${text}`} className="text-gray-300 capitalize hover:text-white transition-colors">{text}</a>
+            })}
           </div>
         </div>
       </div>
